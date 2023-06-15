b9581faccb955e15987c944627962fef31d0e4e1
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-06-16 01:12:44 +0300

    fix

--- decorators/isValidId.js
+++ decorators/isValidId.js
@@ -4,7 +4,7 @@ const { HttpError } = require('../helpers');
 
 const isValidId = (req, res, next) => {
   const { id } = req.params;
-  if (isValidObjectId(id)) {
+  if (!isValidObjectId(id)) {
     next(HttpError(400, `${id} isn't valid id.`));
   }
   next();
